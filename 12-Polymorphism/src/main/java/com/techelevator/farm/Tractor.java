86bd31ed883c956bbc204f7f00a7ccbd19bb37e7
Tom Medvitz <tom@techelevator.com>
2019-01-29 11:48:55 -0500

    Updating polymorphism lecture code

---
+++ 12-Polymorphism/src/main/java/com/techelevator/farm/Tractor.java
@@ -0,0 +1,17 @@
+package com.techelevator.farm;
+
+public class Tractor implements Singable {
+	
+	public Tractor() {		
+		
+	}
+	
+	public String getName() {
+		return "Tractor";
+	}
+	
+	public String getSound() {
+		return "Vroom!";
+	}
+	
+}
